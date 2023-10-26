051a69550707c8df8b0023abe1666019a32f9c1b
Zhifeng Jiang <samuelgong2017@gmail.com>
2023-10-26 11:51:11 +0800

    Trying CloudLab.

---
+++ profile.py
@@ -0,0 +1,27 @@
+"""This is a trivial example of a gitrepo-based profile; The profile source code and other software, documentation, etc. are stored in in a publicly accessible GIT repository (say, github.com). When you instantiate this profile, the repository is cloned to all of the nodes in your experiment, to `/local/repository`. 
+
+This particular profile is a simple example of using a single raw PC. It can be instantiated on any cluster; the node will boot the default operating system, which is typically a recent version of Ubuntu.
+
+Instructions:
+Wait for the profile instance to start, then click on the node in the topology and choose the `shell` menu item. 
+"""
+
+# Import the Portal object.
+import geni.portal as portal
+# Import the ProtoGENI library.
+import geni.rspec.pg as pg
+
+# Create a portal context.
+pc = portal.Context()
+
+# Create a Request object to start building the RSpec.
+request = pc.makeRequestRSpec()
+ 
+# Add a raw PC to the request.
+node = request.RawPC("node")
+
+# Install and execute a script that is contained in the repository.
+node.addService(pg.Execute(shell="bash", command="cd exploration/dev && bash standalone_install.sh"))
+
+# Print the RSpec to the enclosing page.
+pc.printRequestRSpec(request)
